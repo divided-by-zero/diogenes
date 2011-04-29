395995bf9a44ff6d8851813f435434b1b4ec5b32
dirk.stanke <dirk.stanke@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-04-29 21:55:22 +0000

    some improvements

--- trunk/Diogenes/src/de/hsrm/diogenes/connection/Main.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/connection/Main.java
@@ -3,20 +3,26 @@ package de.hsrm.diogenes.connection;
 import java.io.IOException;
 
 import de.fhwiesbaden.webrobbie.wrp.WRPException;
+import de.hsrm.diogenes.logic.Movement;
 import de.hsrm.diogenes.map.Map;
 
 public class Main {
 
 	public static void main(String[] args) throws WRPException, IOException {
-		Client c = new Client();
+		Connection c = new Connection();
 		// settings for the simulator
-//		c.run("localhost", 33333);
+		c.run("localhost", 33333);
 		// settings for the robbie
-		c.run("10.18.72.254", 33333);
-//		Map map = new Map(c);
-//		map.readPacket();
+		//c.run("10.18.72.254", 33333);
+		Map map = new Map(c);
+		Movement move = new Movement(c);
 		
-//		c.moveTo(15, 10);
+		//move.moveForward(5);
+		//move.moveBackward(5);
+		//move.turnLeft(5);
+		//move.turnRight(5);
+		move.moveTo(180, 70);
+		c.getDiogenes().disconnect();
 		//roboterbewegung?
 		//c.turnLeft(23);
 	}	
---
+++ trunk/Diogenes/src/de/hsrm/diogenes/logic/Movement.java
@@ -0,0 +1,91 @@
+package de.hsrm.diogenes.logic;
+
+import de.fhwiesbaden.webrobbie.wrp.WRPCmd;
+import de.fhwiesbaden.webrobbie.wrp.WRPException;
+import de.fhwiesbaden.webrobbie.wrp.packet.WRPCommand;
+import de.hsrm.diogenes.connection.Connection;
+
+public class Movement {
+
+	private Connection c;
+	
+	public Movement(Connection c){
+		this.c = c;
+	}
+	
+	/**
+	 * A method to move the robot to an absolute position
+	 * 
+	 * @param x The x-coordinate on the map
+	 * @param y The y-coordinate on the map
+	 */
+	
+	public void moveTo(int x, int y) {
+		try {
+			int[] coordinates = {x,y};
+			System.out.println("moving...");
+			c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.GOTO_XY, coordinates));
+			//this.getDiogenesconn().requestMove(x, y);
+			c.getDiogenes().waitFor(WRPCmd.GOTO_XY);
+			System.out.println("...moving finished");
+		} catch (WRPException e) {
+			System.err.println("Couldn't move Diogenes to (" + x + "," + y + "):");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Move forward.
+	 *
+	 * @param x 
+	 * @throws WRPException the wRP exception
+	 */
+	public void moveForward(int x) throws WRPException{
+		c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_FORWARD, x));
+		c.getDiogenes().waitFor(WRPCmd.MOVE_FORWARD);
+		System.out.println("Move forward um" +x);
+	}
+	
+	/**
+	 * Move backward.
+	 *
+	 * @param x The distance the robot walks
+	 * @throws WRPException the wRP exception
+	 */
+	public void moveBackward(int x) throws WRPException{
+		c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_BACKWARD, x));
+		c.getDiogenes().waitFor(WRPCmd.MOVE_BACKWARD);
+		System.out.println("Move backward um" +x);
+	}
+	
+	/**
+	 * Turn left.
+	 *
+	 * @param x The angle the robot turns
+	 * @throws WRPException the wRP exception
+	 */
+	public void turnLeft(int x) throws WRPException{
+//		diogenes.requestStopMoving();
+//		diogenes.waitFor(WRPCmd.STOP_MOVING);
+		System.out.println("start rotating");
+		c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_LEFT, x));
+		c.getDiogenes().waitFor(WRPCmd.ROTATE_LEFT);
+		System.out.println("stopped rotating");
+//		System.out.println("Turn Left um" +x);
+		
+	}
+	
+	/**
+	 * Turn right.
+	 *
+	 * @param x The angle the robot turns
+	 * @throws WRPException the wRP exception
+	 */
+	public void turnRight(int x) throws WRPException{
+		c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_RIGHT, x));
+		c.getDiogenes().waitFor(WRPCmd.ROTATE_RIGHT);
+		System.out.println("TurnRight um" +x);
+	}
+	
+}
