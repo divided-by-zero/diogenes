3e311a5a5ae7adefcfae7eda9972e49387531dd0
dirk.stanke <dirk.stanke@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-05-04 13:02:21 +0000

    Map2Image conversion added

--- trunk/Diogenes/src/de/hsrm/diogenes/connection/Main.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/connection/Main.java
@@ -1,10 +1,13 @@
 package de.hsrm.diogenes.connection;
 
 import java.awt.Point;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import de.fhwiesbaden.webrobbie.clientutil.map.Map2ImageTransformer;
 import de.fhwiesbaden.webrobbie.wrp.WRPException;
 import de.hsrm.diogenes.logic.Movement;
+import de.hsrm.diogenes.map.ConvertMap2Image;
 import de.hsrm.diogenes.map.Map;
 
 public class Main {
@@ -16,13 +19,16 @@ public class Main {
 		//c.run("localhost", 33333);
 		// settings for the robbie
 	 	Map map = new Map(c);
-		Movement move = new Movement(c);
+		ConvertMap2Image cv = new ConvertMap2Image(map.getMap(), map.getMap().getMapWidth());
+		
+		
+		//Movement move = new Movement(c);
 		
 		//move.moveForward(50);
 		//move.moveBackward(5);
 		//move.turnLeft(5);
 		//move.turnRight(5);
-		move.wander(new Point(3367, -1747), new Point(6274, 1620), new Point(-334, 1570));
+		//move.wander(new Point(3367, -1747), new Point(6274, 1620), new Point(-334, 1570));
 		
 		//move.moveTo(-334, 1570);
 		c.getDiogenes().disconnect();
