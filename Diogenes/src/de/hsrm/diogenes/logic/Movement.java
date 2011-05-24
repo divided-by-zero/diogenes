32d16f69388b28935ce3cbb8f51dc522806fb960
daniel.ernst01 <daniel.ernst01@googlemail.com>
2011-05-24 10:07:15 +0000

    Movements how they should work when the WRPServer works. Completed so far.

--- Diogenes/src/de/hsrm/diogenes/logic/Movement.java
+++ Diogenes/src/de/hsrm/diogenes/logic/Movement.java
@@ -4,8 +4,6 @@ import java.awt.Point;
 import de.fhwiesbaden.webrobbie.wrp.WRPCmd;
 import de.fhwiesbaden.webrobbie.wrp.WRPException;
 import de.fhwiesbaden.webrobbie.wrp.packet.WRPCommand;
-import de.fhwiesbaden.webrobbie.wrp.packet.WRPRobotInfoPacket;
-import de.fhwiesbaden.webrobbie.wrp.packet.WRPStatusPacket;
 import de.hsrm.diogenes.connection.Connection;
 
 public class Movement {
@@ -42,23 +40,10 @@ public class Movement {
 	public void moveForward(int d) throws WRPException {
 		/* 
 		 * the MOVE_FORWARD WRPCmd is broken in WRP-3.0.2-beta.jar
-		 * this is how it should work:
-		 * 		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_FORWARD, d));
-		 * 		this.c.getDiogenes().waitFor(WRPCmd.MOVE_FORWARD);
-		 * here is a workaround using right-angled-triangle-geometry and the GOTO_XY-WRPCmd:
+		 * this is how it should work: 
 		 */
-		// get robot's current position data
-		int alpha = c.getLocation().getAngle();
-		if (alpha < 0) alpha *= -1;
-		int x = c.getLocation().getX();
-		int y = c.getLocation().getY();
-		// calculating delta of x and y coordinates to target 							TODO: WRONG CALCULATION!
-		int dy = (int) -(Math.cos(alpha) * d);
-		int dx = (int) -(Math.sin(alpha) * d);
-		System.out.println("angle:"+alpha+", x:"+x+", y:"+y+", dx:"+dx+", dy:"+dy);
-		// target-position is current coordinates plus delta-coordinates
-		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.GOTO_XY, x+dx, y+dy));
-		this.c.getDiogenes().waitFor(WRPCmd.GOTO_XY);
+		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_FORWARD, d));
+		this.c.getDiogenes().waitFor(WRPCmd.MOVE_FORWARD);
 	}
 	
 	/**
@@ -71,10 +56,9 @@ public class Movement {
 		/* 
 		 * the MOVE_BACKWARD WRPCmd is broken in WRP-3.0.2-beta.jar
 		 * this is how it should work:
-		 * 		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_BACKWARD, d));
-		 * 		this.c.getDiogenes().waitFor(WRPCmd.MOVE_BACKWARD);
 		 */
-		moveForward(-d);
+		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.MOVE_BACKWARD, d));
+		this.c.getDiogenes().waitFor(WRPCmd.MOVE_BACKWARD);
 	}
 	
 	/**
@@ -84,14 +68,12 @@ public class Movement {
 	 * @throws WRPException the wRP exception
 	 */
 	public void turnLeft(int x) throws WRPException {
-		// TODO: doesn't work at all, WRPCmd broken
-//		diogenes.requestStopMoving();
-//		diogenes.waitFor(WRPCmd.STOP_MOVING);
-//		System.out.println("start rotating");
-//		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_LEFT, x));
-//		this.c.getDiogenes().waitFor(WRPCmd.ROTATE_LEFT);
-//		System.out.println("stopped rotating");
-//		System.out.println("Turn Left um" +x);
+		/* 
+		 * the TURN_LEFT WRPCmd is broken in WRP-3.0.2-beta.jar
+		 * this is how it should work:
+		 */
+		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_LEFT, x));
+		this.c.getDiogenes().waitFor(WRPCmd.ROTATE_LEFT);
 	}
 	
 	/**
@@ -101,10 +83,12 @@ public class Movement {
 	 * @throws WRPException the wRP exception
 	 */
 	public void turnRight(int x) throws WRPException {
-		// TODO: doesn't work at all, WRPCmd broken
-//		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_RIGHT, x));
-//		this.c.getDiogenes().waitFor(WRPCmd.ROTATE_RIGHT);
-//		System.out.println("TurnRight um" +x);
+		/* 
+		 * the TURN_RIGHT WRPCmd is broken in WRP-3.0.2-beta.jar
+		 * this is how it should work:
+		 */
+		this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.ROTATE_RIGHT, x));
+		this.c.getDiogenes().waitFor(WRPCmd.ROTATE_RIGHT);
 	}
 	
 	/**
@@ -115,11 +99,10 @@ public class Movement {
 	 */
 	public void wander(Point... p) throws WRPException {
 		// works fine
-		for(Point pp : p){
+		for(Point pp : p) {
 			this.c.getDiogenes().sendCommand(new WRPCommand(WRPCmd.GOTO_XY, (int)pp.getX(), (int)pp.getY()));
 			this.c.getDiogenes().waitFor(WRPCmd.GOTO_XY);
 		}
-		System.out.println("All points visited O_o");
 	}
 	
 	
