62d7a814122efc0b63c9db4080d0d447d57112db
daniel.ernst01@googlemail.com <daniel.ernst01@googlemail.com@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-05-30 10:39:20 +0000

    making the socket-classes ready for object-send-and-receive and OO-architecture

--- trunk/Diogenes/src/de/hsrm/diogenes/socket/Server.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/socket/Server.java
@@ -1,49 +1,67 @@
 package de.hsrm.diogenes.socket;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+//TODO proper exception handling!!
 public class Server {
 
-	private ServerSocket server;
+	private ServerSocket server_sock;
+	
+	private Socket current_client;
+	
+	private ObjectInputStream client_input;
+	
+	/**
+	 * Output from server to client not necessary or used here yet.
+	 */
+	private ObjectOutputStream client_output;
+	
+	private PresentationPacket packet;
 	
 	public Server(int port) throws IOException {
-		server = new ServerSocket(port);
-		listen();
+		System.out.println("server: initializing serversocket");
+		server_sock = new ServerSocket(port);
 	}
 
-	private void listen() {
-		while (true) {
-			Socket client;
-			try {
-				client = server.accept();
-				InputStream input = client.getInputStream();
-				OutputStream output = client.getOutputStream();
-				// simple tests with integers first, needs to be done for objects!
-				int z1 = input.read();
-				int z2 = input.read();
-				output.write(z1 + z2);
-				output.flush();
-				input.close();
-				output.close();
-			} catch (IOException e) {
-				System.err.println("Error handling client");
-				e.printStackTrace();
-			}
-		}
+	public void waitForClient() throws IOException {
+		System.out.println("server: accepting clients...");
+		current_client = server_sock.accept();
+		client_input = new ObjectInputStream(current_client.getInputStream());
+		client_output = new ObjectOutputStream(current_client.getOutputStream());
+		System.out.println("server: client accepted, holding connection");
 	}
 	
-	public static void main(String[] args) {
-		try {
-			System.out.println("starting server");
-			new Server(55555);
-			System.out.println("server closed");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void closeCurrentClient() throws IOException {
+		System.out.println("server: closing current client");
+		client_input.close();
+		client_output.close();
+		current_client.close();
 	}
+	
+	public void send() throws IOException {
+		System.out.println("server: sending current presentationpacket back to client");
+		client_output.writeObject(packet);
+		client_output.flush();
+	}
+	
+	public void send(PresentationPacket packet) throws IOException {
+		System.out.println("server: sending new presentationpacket back to client");
+		client_output.writeObject(packet);
+		client_output.flush();
+	}
+	
+	public void recieve() throws IOException, ClassNotFoundException {
+		System.out.println("server: recieving presentationpacket");
+		packet = (PresentationPacket) client_input.readObject();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Server s = new Server(55555);
+		s.waitForClient();
+	}
+
 }
