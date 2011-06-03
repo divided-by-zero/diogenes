d1ef15ea39984ab90b6b58ebe1b48fa733135942
daniel.ernst01 <daniel.ernst01@googlemail.com>
2011-06-03 21:35:22 +0000

    cleaning and polishing code, adding further comments

--- Diogenes/src/de/hsrm/diogenes/remotepresentation/Server.java
+++ Diogenes/src/de/hsrm/diogenes/remotepresentation/Server.java
@@ -1,75 +1,104 @@
 package de.hsrm.diogenes.remotepresentation;
 
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.ImageIcon;
 
+/**
+ * A Server-Object will be connected to a Client-Object via a network.
+ * It's use is to display information of Packets sent by the Client-Object.
+ * This class will start connecting using the run-method, which
+ * also runs this Object in an own Thread.
+ * @author Daniel Ernst
+ */
 public class Server extends Thread {
 
 	/**
-	 * A packet containing a picture and a text to be displayed by a GUI (use
-	 * getPacket). Will be updated by a remote client using sockets.
+	 * A packet containing an image and a text to be displayed by a GUI (use
+	 * getPacket). Will be updated by a remote client.
 	 */
 	private Packet packet;
-
+	
+	/**
+	 * The port to be used for the connection with Clients
+	 */
 	private int port;
-
-	public Server(int port) {
+	
+	/**
+	 * A shared Object of ServerGUI and Server, so that the Server
+	 * as a Thread is able to throw an Exception to the ExceptionListener
+	 * and the ServerGUI can read the Servers' Exception out of it.
+	 * Also used as a lock for synchronizing ServerGUI and Server-Thread
+	 */
+	private ExceptionListener exceptionlistener;
+	
+	/**
+	 * Instantiates the Server at the port.
+	 * The ExceptionListener should be owned by a class
+	 * holding a Server-Object and given to the Server.
+	 * This way the class holding the Server (e.g. the GUI)
+	 * can get Exceptions thrown by the Server. Casual
+	 * java-Exceptions won't work as this Server runs in an
+	 * own Thread. The ExceptionListener will be used as
+	 * a lock for synchronization as well. 
+	 * @param port The port to be opened
+	 * @param el An ExceptionListener of the class 
+	 * 			holding this Object (e.g. a GUI)
+	 */
+	public Server(int port, ExceptionListener el) {
 		this.port = port;
+		this.exceptionlistener = el;
+		// first Packet will be a "welcome-packet"
 		packet = new Packet(new ContentImpl(
 				new ImageIcon("example.jpg"), 
 				"<html><B>No Information gathered so far...</B>", 
 				0, 0, 0, 0));
 	}
 
+	/**
+	 * Runs the Server in an own Thread and starts accepting
+	 * Clients (infinite).
+	 * Notifies any Exceptions to the ExceptionListener after doing so
+	 * and opens the lock with doing that.
+	 */
 	public void run() {
-		System.out.println("server: initializing serversocket");
 		ServerSocket server_sock;
 		try {
 			server_sock = new ServerSocket(port);
+			// connection with port successful, notify
+			synchronized (exceptionlistener) {
+				exceptionlistener.notify();
+			}
 			// infinite loop for accepting clients, receiving data and disconnecting them
 			while (true) {
 				// accept client
-				System.out.println("server: accepting clients...");
 				Socket current_client = server_sock.accept();
 				ObjectInputStream client_input = new ObjectInputStream(current_client.getInputStream());
-				System.out.println("server: client accepted, holding connection");
 				// receive data
-				System.out.println("server: receiving presentationpacket");
 				packet = (Packet) client_input.readObject();
-				// show what you got
-				System.out.println("server: stored: " + 
-										packet.getContent().getImage() + ", " + 
-										packet.getContent().getDescriptionText());
 				// close connection
 				current_client.close();
 				client_input.close();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (Throwable t) {
+			exceptionlistener.notifyException(t);
+		}
+		// due to the whileloop this part of code is only reachable when 
+		// an exception occurred, then notify
+		synchronized (exceptionlistener) {
+			exceptionlistener.notify();
 		}
 	}
 
 	/**
-	 * Returns a packet containing a picture and a text to be displayed by a
-	 * GUI. Will be updated by a remote client using sockets.
-	 * 
-	 * @return The packet containing a picture and a text for visualization
+	 * Returns a packet containing an image and a text to be displayed by a
+	 * GUI. Will be updated by a remote Client.
+	 * @return The packet containing an image and a text for visualization
 	 */
 	public Packet getPacket() {
 		return packet;
-	}
-
-	public static void main(String[] args) {
-		Server s = new Server(55555);
-		s.start();
 	}
 
 }
