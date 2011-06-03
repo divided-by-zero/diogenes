9b51aeaf898c97b25f310ae9593aa62fa35ac615
daniel.ernst01@googlemail.com <daniel.ernst01@googlemail.com@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-06-03 21:01:05 +0000

    fixing little bugs (exceptionhandling), polishing code, adding lots of comments

--- trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/Client.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/Client.java
@@ -3,49 +3,115 @@ package de.hsrm.diogenes.remotepresentation;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
+/**
+ * A Client-Object connects to a Server-Object via a network.
+ * It's use is to send packages with information to the Server,
+ * who presents it on a display for example.
+ * This class will start connecting using the run-method, which
+ * also runs this Object in an own Thread.
+ * @author Daniel Ernst
+ */
 public class Client extends Thread {
 
+	/**
+	 * Holds the destination address (Server)
+	 */
 	private String dest_addr;
+	
+	/**
+	 * Holds the port
+	 */
 	private int port;
+	
+	/**
+	 * A shared Object of the Client and its GUI, so that the Client
+	 * as a Thread is able to throw a connection to the ExceptionListener
+	 * and the GUI can read the Clients' Exception out of it.
+	 * Also used as a lock for synchronizing GUI and Client-Thread
+	 */
 	private ExceptionListener exceptionlistener;
+	
 	/**
 	 * Holding the socket-connection
 	 */
 	private Socket server;
+	
 	/**
-	 * Holding the output-stream (for PresentationPacket-Objects)
+	 * Holding the output-stream (for Packet-Objects)
 	 */
 	private ObjectOutputStream output;
 	
+	/**
+	 * Instantiates the Client with the address.
+	 * The ExceptionListener should be owned by a class
+	 * holding a Client-Object and given to the Client.
+	 * This way the class holding the client (e.g. the GUI)
+	 * can get Exceptions thrown by the Client. Casual
+	 * java-Exceptions won't work as this Client runs in an
+	 * own Thread. The ExceptionListener will be used as
+	 * a lock for synchronization as well. 
+	 * @param dest_addr The address to the host (Server)
+	 * @param port The port of the host (Server)
+	 * @param el An ExceptionListener of the class 
+	 * 			holding this Object (e.g. a GUI)
+	 */
 	public Client(String dest_addr, int port, ExceptionListener el) {
 		this.dest_addr = dest_addr;
 		this.port = port;
 		this.exceptionlistener = el;
 	}
 	
+	/**
+	 * Instantiates the Client with the address.
+	 * An empty ExceptionListener will be used as default.
+	 * Note that most of the Exceptions of this Client
+	 * will then be lost.
+	 * @param dest_addr The address to the host (Server)
+	 * @param port The port of the host (Server)
+	 */
+	public Client(String dest_addr, int port) {
+		this.dest_addr = dest_addr;
+		this.port = port;
+		this.exceptionlistener = new ExceptionListener();
+	}
+
+	/**
+	 * Runs the Client in an own Thread and starts connecting
+	 * to the address of the Server.
+	 * Notifies any Exceptions to the ExceptionListener after doing so
+	 * and opens the lock with doing that.
+	 */
 	public void run() {
-		System.out.println("client: connecting");
 		try {
 			server = new Socket(dest_addr, port);
 			output = new ObjectOutputStream(server.getOutputStream());
 			output.flush();
-			System.out.println("client: connection established");
-		} catch (UnknownHostException e) {
-			System.out.println("client throwing: " + e.getMessage());
-			exceptionlistener.notifyException(e);
-		} catch (IOException e) {
-			System.out.println("client throwing: " + e.getMessage());
-			exceptionlistener.notifyException(e);
+		} catch (Throwable t) {
+			exceptionlistener.notifyException(t);
 		}
+		synchronized (exceptionlistener) {
+			exceptionlistener.notify();
+		}
+	}
+
+	/**
+	 * Disconnects the client from the server and closes the streams.
+	 * Notifies any Exceptions to the ExceptionListener after doing so
+	 * and opens the lock with doing that.
+	 * @throws IOException If closing the socket or the stream didn't work
+	 */
+	public void disconnect() throws IOException {
+		System.out.println("client: disconnecting");
+		server.close();
+		output.close();
 		synchronized (exceptionlistener) {
 			exceptionlistener.notify();
 		}
 	}
 	
 	/**
-	 * Sends a PresentationPacket to the Server who stores it locally.
+	 * Sends a Packet to the Server who stores it locally.
 	 * @param packet The packet to be sent to the Server
 	 * @throws IOException If streams within the connection couldn't be established
 	 */
@@ -54,27 +120,5 @@ public class Client extends Thread {
 		output.writeObject(packet);
 		output.flush();
 	}
-
-	/**
-	 * Disconnects the client from the server and closes the streams.
-	 * @throws IOException If closing the socket or the stream didn't work
-	 */
-	public void disconnect() throws IOException {
-		System.out.println("client: disconnecting");
-		server.close();
-		output.close();
-	}
-	
-//	public static void main(String[] args) throws ClassNotFoundException {
-//		Client c = new Client("localhost", 55555);
-//		try {
-//			c.start();
-//			c.send(new Packet(new ContentImpl(new ImageIcon("example.jpg"), "that's new!", 17, 4, 0, 42)));
-//			c.disconnect();
-//		} catch (IOException e) {
-//			System.err.println("Streams couldn't be established");
-//			e.printStackTrace();
-//		}
-//	}
 	
 }
--- trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/ClientGUI.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/ClientGUI.java
@@ -4,32 +4,94 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.swing.Timer;
 
+/**
+ * This GUI is a comfortable way of using a Client-Object
+ * for a remote presentation on a Server-Object.
+ * @see Client
+ * @author Daniel Ernst
+ */
 public class ClientGUI extends JFrame {
 
+	/**
+	 * SerialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Holds a Client-Object who connects to a Server-Object
+	 */
 	private Client client;
+	
+	/**
+	 * A shared Object of ClientGUI and Client, so that the Client
+	 * as a Thread is able to throw a connection to the ExceptionListener
+	 * and the ClientGUI can read the Clients' Exception out of it.
+	 * Also used as a lock for synchronizing ClientGUI and Client-Thread
+	 */
 	private ExceptionListener exceptionlistener;
+	
+	/**
+	 * Represents if the connection between this Client and a Server
+	 * is/has been established
+	 */
 	boolean connected;
+	
+	/**
+	 * A container holding Contents to be transferred via network in a
+	 * Packet to the Server and visualized there, if a certain value in
+	 * the Content is reached.
+	 */
 	private ContentContainer container;
+	
+	/**
+	 * The input-field for the hostaddress (Server)
+	 */
 	private JTextField hostfield;
+	
+	/**
+	 * The input-field for the hostaddress (Server)
+	 */
 	private JTextField portfield;
+	
+	/**
+	 * The input-field for the container-file
+	 */
 	private JTextField containerfield;
+	
+	/**
+	 * Shows the current state of the program (connected and running
+	 * or disconnected)
+	 */
 	private JLabel status;
+	
+	/**
+	 * The only button, initally a "Connect" button, but on an established
+	 * connection it turns to the "Disconnect" button
+	 */
 	private JButton button;
 
+	/**
+	 * Instantiates the GUI values and starts the main window
+	 */
 	public ClientGUI() {
-		// internal stuff
 		connected = false;
 		exceptionlistener = new ExceptionListener();
+		startMainWindow();
+	}
+
+	/**
+	 * Builds the GUI's main window with initial values in the fields
+	 * and sets the button's ActionListener which starts the
+	 * connect() or disconnect()-methods
+	 */
+	private void startMainWindow() {
 		// set up main frame
-		this.setTitle("Presentation Window");
+		this.setTitle("Presentation Window (Client)");
 		this.setLayout(new GridLayout(4, 2));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// center on screen
@@ -54,7 +116,7 @@ public class ClientGUI extends JFrame {
 				}
 			}
 		});
-		// assemble window
+		// assemble window and show
 		this.add(hostlabel);
 		this.add(hostfield);
 		this.add(portlabel);
@@ -63,61 +125,107 @@ public class ClientGUI extends JFrame {
 		this.add(containerfield);
 		this.add(status);
 		this.add(button);
-		// finish
 		this.pack();
 		this.setVisible(true);
 	}
-
+	
+	/**
+	 * Instantiates the ContentContainer.
+	 * Instantiates the Client with the values of the inputfields and
+	 * shares the ExceptionListener with him. Starts the Client in
+	 * an own Thread, but will wait for the Client to finish the
+	 * initialization using the ExceptionListener as a lock.
+	 * Will show errormessages to the user if the Client-Thread
+	 * notified an Exception to the ExceptionListener.
+	 * Changes the button whether to a "Connect"- or "Disconnect"-
+	 * button and the statuslabel as well.
+	 */
 	private void connect() {
 		// TODO set up container
 		container = new ContentContainer();
 		// set up client for receiving packets
 		client = new Client(getHost(), getPort(), this.exceptionlistener);
+		// start in own thread
 		client.start();
-		// TODO: workaround - not finished yet. waiting for client to set exception
+		// waiting for client-thread to finish initialization (exceptions possible)
 		synchronized (exceptionlistener) {
 			try {
 				exceptionlistener.wait();
 			} catch (InterruptedException e) {
-				System.out.println("whoops");
+				// ignore interruptions as the client does not do it
+				// but warn the user
+				JOptionPane.showMessageDialog(
+						new JFrame(), 
+						e.getMessage() + "\nClients' synchronization interrupted:\n" +
+								"This may cause bad Exception-Handling", 
+						"Warning", 
+						JOptionPane.WARNING_MESSAGE, null);
 			}
 		}
-		// check for occurred exception in thread
+		// waiting done, check if exception occurred in the client
 		try {
 			// throws exception if client got exception, else does nothing
-			System.out.println("GUI: getting exceptions...");
 			exceptionlistener.throwException();
-			// if no exception do this
+			// if no exception, it's connected, 
+			// so set the buttons and labels correctly
 			connected = true;
 			button.setText("Disconnect");
 			status.setText("connected/running");
 		} catch (Throwable e1) {
-			// popup exception, don't change the button+label
+			// popup exception, don't change the button and label
 			System.out.println("GUI: popping exceptions...");
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE, null);
+			JOptionPane.showMessageDialog(
+									new JFrame(), 
+									e1.getMessage(), 
+									"Error", 
+									JOptionPane.ERROR_MESSAGE, null);
 		}
 	}
 	
+	/**
+	 * Disconnects the Client from the Server, shows error-
+	 * messages if Exception in the Client occurred (does
+	 * not use the ExceptionListener for notification).
+	 * Sets the button appropriate to whether "Connected"
+	 * or "Disconnected".
+	 */
 	private void disconnect() {
-		// check for occurred exception in thread
+		// check if exception occurred in the client
 		try {
+			// disconnect may throw an exception
 			client.disconnect();
-			// throws exception if client got exception, else does nothing
-			exceptionlistener.throwException();
-			// if no exception do this
+			// no synchronized procedure to check for exceptions via
+			// exceptionlistener is needed here as the method disconnect
+			// is able to throw directly.
+			// if no exception, it's disconnected, 
+			// so set the buttons and labels correctly
 			connected = false;
 			button.setText("Connect");
 			status.setText("disconnected");
 		} catch (Throwable e1) {
 			// popup exception, don't change the button+label
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE, null);
-		}		
+			JOptionPane.showMessageDialog(
+									new JFrame(), 
+									e1.getMessage(), 
+									"Error", 
+									JOptionPane.ERROR_MESSAGE, 
+									null);
+		}
 	}
 	
+	/**
+	 * Returns the current hostaddress of the hostfield
+	 * @return The current input of the hostfield
+	 */
 	private String getHost() {
 		return hostfield.getText();
 	}
-	
+
+	/**
+	 * Returns the current port of the portfield
+	 * (converts the String to Integer)
+	 * @return The current input of the portfield
+	 */
 	private Integer getPort() {
 		return Integer.valueOf(portfield.getText());
 	}
