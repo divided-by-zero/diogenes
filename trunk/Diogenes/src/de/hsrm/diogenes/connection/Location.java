f2fc723ba24bac7368496c26151f348d750631b7
daniel.ernst01@googlemail.com <daniel.ernst01@googlemail.com@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-06-07 14:42:35 +0000

    added some methods to location

--- trunk/Diogenes/src/de/hsrm/diogenes/connection/Location.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/connection/Location.java
@@ -12,6 +12,27 @@ public class Location {
 		this.angle = angle;
 	}
 	
+	public String toString() {
+		return "Location: X=" + x + ", Y=" + y + ", Angle=" + angle; 
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void setAll(int x, int y, int angle) {
+		setX(x);
+		setY(y);
+		setAngle(angle);
+	}
+	
+	public void setAngle(int angle) {
+		this.angle = angle;
+	}
 	public int getX() {
 		return x;
 	}
