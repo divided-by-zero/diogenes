3bd09fa005abfd7ef282ef6a19ea33df35b47c6d
daniel.ernst01@googlemail.com <daniel.ernst01@googlemail.com@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-07-01 10:35:45 +0000

    presentationmode now can send image as well (via bytearrays)

--- trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/ServerGUI.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/ServerGUI.java
@@ -4,8 +4,6 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
