00ae41f86e83587654a5eb299664b55a34c3bb8c
daniel.ernst01@googlemail.com <daniel.ernst01@googlemail.com@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-07-26 16:43:06 +0000

    Changed PacketContainer from an exampleclass to a usable class

--- trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/PacketContainer.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/remotepresentation/PacketContainer.java
@@ -1,7 +1,5 @@
 package de.hsrm.diogenes.remotepresentation;
 
-import java.awt.Rectangle;
-import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -9,39 +7,16 @@ import java.util.ArrayList;
  */
 public class PacketContainer extends ArrayList<Presentable> {
 
-	/** The Constant serialVersionUID. */
+	/** The constant serialVersionUID. */
 	private static final long serialVersionUID = -868542116216859181L;
 
 	/**
-	 * Instantiates a new packet container.
+	 * Instantiates a new packet container with the given presentables
 	 */
-	public PacketContainer() {
-		// TODO add items by user
-		// testitems:
-		// 1. rectangle at starting position of robbie
-		this.add(new Packet(
-				new File("test1.jpg"), 
-				"The answer is 42", 
-				new Rectangle(-1000, 1000, 1300, 1300)));
-		//2. rectangle in front of the middle-desk
-		this.add(new Packet(
-				new File("test3.jpg"), 
-				"<html><B>Kaffee</B> [ˈkafe, kaˈfeː] (türk. kahve aus arab. ‏قهوة‎ qahwa) " +
-				"ist ein schwarzes, coffeinhaltiges Heißgetränk, das aus gerösteten " +
-				"(weshalb man auch von Röstkaffee spricht) und gemahlenen Kaffeebohnen " +
-				"hergestellt wird.</html>",
-				new Rectangle(1000, -2000, 3500, 2000)));
-//		this.add(new PaketFurz( 
-//		"The answer is 42",
-//		"zweiter",
-//		new Rectangle(-1000, 1000, 1300, 1300)));
-//		this.add(new PaketFurz( 
-//		"<html><B>Kaffee</B> [ˈkafe, kaˈfeː] (türk. kahve aus arab. ‏قهوة‎ qahwa) " +
-//		"ist ein schwarzes, coffeinhaltiges Heißgetränk, das aus gerösteten " +
-//		"(weshalb man auch von Röstkaffee spricht) und gemahlenen Kaffeebohnen " +
-//		"hergestellt wird.</html>",
-//		"zweiter",
-//		new Rectangle(1000, -2000, 3500, 2000)));
+	public PacketContainer(Presentable... presentables) {
+		for (Presentable p : presentables) {
+			this.add(p);
+		}
 	}
 	
 }
