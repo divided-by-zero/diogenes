a29497fb01eb7871dcc0841be02433bbe17eedf9
oliver-kieven@gmx.de <oliver-kieven@gmx.de@55911cb0-55e3-1b35-f555-b69a5016a6a2>
2011-07-27 10:03:29 +0000

    Now you can train person's to face.com via menu

--- trunk/Diogenes/src/de/hsrm/diogenes/faceClientExample/ClientExample.java
+++ trunk/Diogenes/src/de/hsrm/diogenes/faceClientExample/ClientExample.java
@@ -1,9 +1,14 @@
 package de.hsrm.diogenes.faceClientExample;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 import com.github.mhendred.face4j.DefaultFaceClient;
 import com.github.mhendred.face4j.FaceClient;
@@ -24,10 +29,10 @@ public class ClientExample
 	/** The Constant API_SEC. */
 	private static final String API_SEC = "c6103059b1be11631fca3c6eda64460d";
 
-//	private static final String URL_WITH_FACES = "/home/olli/Desktop/Bilder/DSCF0493.jpg";
+	private static final String PHOTOPATH = "/home/olli/workspace/Diogenes/photo1";
 
 	/** The Constant NAMESPACE. */
-private static final String NAMESPACE = "Diogenes";
+	private static final String NAMESPACE = "Diogenes";
 
 	/** The Constant USER. */
 	private static final String USER = "Oliver_Kieven";
@@ -41,6 +46,58 @@ private static final String NAMESPACE = "Diogenes";
 	 * @throws FaceClientException the face client exception
 	 * @throws FaceServerException the face server exception
 	 */
+	public static void trainPerson () throws FaceClientException, FaceServerException {
+		final JFrame frame = new JFrame("Name input");
+		final JTextField tf = new JTextField();
+		JButton button = new JButton("Submit");
+		frame.add(tf);
+		frame.add(button);
+		frame.setLayout(new GridLayout());
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					
+				Thread t = new Thread(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							FaceClient faceClient = new DefaultFaceClient(API_KEY, API_SEC);
+							final String userlabel = tf.getText();
+							File file = new File(PHOTOPATH);
+							Photo photo = faceClient.detect(file);
+							/*
+					    	 * Now we pull out the temporary tag and call save with the desired username and label.
+					    	 */
+							Face f = photo.getFace();
+					    	faceClient.saveTags(f.getTID(), USER_ID, userlabel);
+					    	/*
+					    	 * Let get the training status for this user now. We should see training in progress TRUE
+					    	 * because we havent called train yet.
+					    	 */
+							faceClient.status(USER_ID);
+							/*
+					    	 * IMPORTANT: Now we call train on our untrained user. This will commit our saved tag for this user to
+					    	 * the database so we can recognize them later with 'recognize' calls   
+					    	 */
+							faceClient.train(USER_ID);
+							frame.dispose();
+						} catch (FaceClientException e1) {
+							e1.printStackTrace();
+						} catch (FaceServerException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
+				t.start();
+			}
+		});
+	}
+	
 	public static void startDetection() throws FaceClientException, FaceServerException{
 		
 		FaceClient faceClient = new DefaultFaceClient(API_KEY, API_SEC);
@@ -54,42 +111,20 @@ private static final String NAMESPACE = "Diogenes";
     	 * @see http://developers.face.com/docs/api/faces-detect/
     	 * @see http://developers.face.com/docs/api/faces-recognize/
     	 */
-    	File file = new File("src/B_000002.jpg");
+    	File file = new File(PHOTOPATH);
     	Photo photo = faceClient.detect(file);
-//    	Photo photo = faceClient.detect(file).get(0);
-    
-    	/*
-    	 * Now we pull out the temporary tag and call save with the desired username and label.
-    	 */
-    	Face f = photo.getFace();
-    	faceClient.saveTags(f.getTID(), USER_ID, "a label");
-    	
-    	/*
-    	 * Let get the training status for this user now. We should see training in progress TRUE
-    	 * because we havent called train yet.
-    	 */
-    	faceClient.status(USER_ID);
-    	
-    
-    	/*
-    	 * IMPORTANT: Now we call train on our untrained user. This will commit our saved tag for this user to
-    	 * the database so we can recognize them later with 'recognize' calls   
-    	 */
-//    	faceClient.train(USER_ID);
-    	
     	/**
     	 * Now we can call recognize. Look for any user in our index (we only have one now)
     	 * We should see a guess now
     	 */
-//    	photo = faceClient.recognize(URL_WITH_FACES, "all@" + NAMESPACE).get(0);
     	photo = faceClient.recognize(file, "all@" + NAMESPACE);
     	
     	for (Face face : photo.getFaces()) {
     		JOptionPane.showMessageDialog(new JFrame(),
-    				"The person on the foto is : " + face.getGender() + "\n" +
+    				"The person on the foto is: " + face.getGender() + "\n" +
     				"Is the person wearing some glasses? " + (face.isWearingGlasses() ? "Yes" : "No") + "\n" +
     				"He/She is smiling? " + (face.isSmiling() ? "Yes" : "No") + "\n\n" +
-    				face.getGuess(), "Face Detection", JOptionPane.INFORMATION_MESSAGE		
+    				"The name of the person is: " + face.getLabel(), "Face Detection", JOptionPane.INFORMATION_MESSAGE		
     		);
     	}
 	}
